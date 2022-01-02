d76b15c2544fa19c631065529639bc012c51a364
Lucas Oliveira <0liv3ir4.luc4s@gmail.com>
2022-01-02 02:48:58 -0300

    Add default redirection from index to login page or 404 page

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    String page = request.getParameter("page").toString();
+	    if (page.equals("login")) {
+		request.getRequestDispatcher("login.jsp").forward(request, response);
+	    } else {
+		request.getRequestDispatcher("404.jsp").forward(request, response);
+	    }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/404.jsp
@@ -0,0 +1,5 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:import url="includes/header.jsp"><c:param name='titulo' value="Error"></c:param></c:import>
+<a href="http://localhost/SignInW-Session/" class="btn btn-secondary ms-3 mt-3">Go back</a>
+<h1 class="text-center my-5">Error</h1>
+<c:import url="includes/footer.jsp"></c:import>
